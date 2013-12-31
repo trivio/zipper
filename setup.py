1d95a2c980c414eb08f4716b5fd855594611c0f5
Scott Robertson <srobertson@codeit.com>
2013-12-31 11:08:01 -0800

    version bump

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError:
 setup(
   name='zipper',
   py_modules = ['zipper'],
-  version='0.0.1',
+  version='0.0.2',
   description=('Functional hierarchical zipper, with navigation, '
                'editing, and enumeration.  See Huet'),
   long_description=README,
